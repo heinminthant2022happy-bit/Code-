45ec27a3dd9913a4b269c40cba1f307db025b52c
heinminthant2022happy-bit <heinminthant2022happy@gmail.com>
2026-04-26 19:30:07 +0630

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ import aa
 
 if name == "main":
     try:
-        # Execute the main function from compiled aa.so
         aa.main()
-    except AttributeError:
-        print("Error: Function 'main' not found in aa.so")
     except Exception as e:
-        print(f"Runtime Error: {e}")
+        print(f"Error: {e}")
